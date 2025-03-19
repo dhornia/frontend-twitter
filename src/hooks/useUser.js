7a6ed0d2ea8b316d53e7dc1ff698a9b5cf1a25cc
Darlen Hornia Puentes <hornia84@gmail.com>
2025-03-19 11:04:11 -0300

    fix useUser hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -9,11 +9,15 @@ function useUser() {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/user/${user.id}`, {
+        const response = await axios({
+          url: `${import.meta.env.VITE_API_URL}/user/${user.userId}`,
+          method: "GET",
           headers: {
-            Authorization: `Bearer ${user.accessToken}`,
+            Authorization: `Bearer ${user.accessToken}}`,
           },
         });
+        console.log(response);
+
         setUserData(response.data);
       } catch (err) {
         console.log(err.message);
